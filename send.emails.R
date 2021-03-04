08efa1758d53ecd2271e655e59b893273eea992f
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2021-03-04 17:31:13 +0800

    created send.mail function

--- send.emails.R
+++ send.emails.R
@@ -1,14 +1,28 @@
 
-#library("devtools")
-#install_github('omegahat/RDCOMClient')
-library(RDCOMClient)
-function.send.email=function(to,subject,body,Attachment)
+#devtools::install_github("dkyleward/RDCOMClient")
+
+# Load the DCOM library
+library (RDCOMClient)
+
+send.email=function(TO,Subject,Body,Attachment=NULL)
 {
-  OutApp <- COMCreate("Outlook.Application")
-  outMail = OutApp$CreateItem(0)
-  outMail[["To"]] = to
-  outMail[["subject"]] = subject
-  outMail[["body"]] = body
-  if(!is.null(Attachment))outMail[["Attachments"]]$Add(Attachment)
-  outMail$Send()
+  # Open Outlook
+  Outlook <- COMCreate("Outlook.Application")
+  
+  # Create a new message
+  Email = Outlook$CreateItem(0)
+  
+  # Set the recipient, subject, and body
+  Email[["to"]] = TO
+  Email[["cc"]] = "matias.braccini@dpird.wa.gov.au"
+  Email[["bcc"]] = ""
+  Email[["subject"]] = Subject
+  Email[["body"]] = Body
+  if(!is.null(Attachment))Email[["attachments"]]$Add(Attachment)  
+  
+  # Send the message
+  Email$Send()
+  
+  # Close Outlook, clear the message
+  rm(Outlook, Email)
 }
