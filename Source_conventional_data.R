8b219b375b15f067f5a12a414b375bc2d1a02f7a
Matias Braccini <Matias.Braccini@dpird.wa.gov.au>
2026-01-16 11:44:52 +0800

    xx

--- Source_conventional_data.R
+++ Source_conventional_data.R
@@ -500,6 +500,9 @@ Tagging=Tagging%>%
                    Recaptured=capitalize(tolower(Recaptured)))%>%
             dplyr::select(-c(dummy,dummy.CAP_FL,dummy.Day.rec,dummy.Mn.rec,dummy.Yr.rec,dummy.Lat.rec,dummy.Long.rec))
 
+Tagging=Tagging%>%
+  mutate(Rec.method=ifelse(!is.na(Rec.method) & Recaptured=='No',NA,Rec.method))
+
 #fix species names
 Tagging$SPECIES=with(Tagging,ifelse(SPECIES=="LP","ZE",
                              ifelse(SPECIES=="DF","SD",
